eabac2648e8a49b7a88986f7eacba4efabb25fbc
Swarnalata-Rana <96971336+Swarnalata-Rana@users.noreply.github.com>
2023-05-16 19:04:40 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -18,4 +18,16 @@ const prom=new Promise((resolve,reject)=>{
 // o/p-yes
 
 //promise are useing callback hell handel
+//callback hell code
 
+setTimeout(()=>{
+    console.log("Called to HR");
+    setTimeout(()=>{
+        console.log("HR to Directer");
+        setTimeout(()=>{
+            cosole.log("Information Update");
+        },1000);
+    },1000);
+},2000);
+
+//Overcame kariba pain Promise use karantni.
